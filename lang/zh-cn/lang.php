609c207b803041e678679066ba7df5b2ae8470fa
lainme <lainme993@gmail.com>
2022-04-07 12:54:27 +0800

    add translation (partial)

---
+++ lang/zh-cn/lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * English language file for template
+ *
+ *  @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+
+$lang['direct_prefix']              = '调转到';
+$lang['direct_content_main']        = '主要内容';
+$lang['direct_quick_search']        = '搜索';
+$lang['direct_content_toggle']      = '改变内容宽度';
+
+$lang['adjunct_linked_logo_text']   = ' - 主页链接';
+$lang['a11y_sidebartoggle']         = '显示/隐藏导航';
+
+$lang['nav-area-head']              = '目录和快速搜索';
+$lang['head_magic_matcher']         = '自动匹配';
+$lang['head_quick_search']          = '快速搜索';
+$lang['head_menu_status']           = '站点状态';
+$lang['head_breadcrumb_youarehere'] = '位置指引';
+$lang['head_menu_trace']            = '最后浏览的页面';
+$lang['head_meta_box']              = '页面元数据';
+$lang['head_menu_starred']          = '加星标的页面';
+$lang['head_menu_tool-select']      = '工具';
+
+$lang['jump_to_quicksearch']        = '调转到快速搜索';
+
+/** it will be shown from plugin lang.php:
+$lang['meta_box_jira_tickets_none'] = 'jira associated tickets found: none';
+$lang['meta_box_tags_none']         = 'tags found: none';
+$lang['js']['meta_box_toc_none']    = 'no Table of Contents available';
+$lang['prefix_tasks_user']          = 'Your open tasks: ';
+*/
+
+$lang['tab_tags']                   = '标签';
+$lang['tab_issues']                 = '问题';
+
+$lang['quality_trigger']            = '显示/隐藏页面分析';
+
+$lang['prefix_tasks_page']          = '打开此页面的任务：';
+
+$lang['tasks_page_none']            = '此页面没有未完成的任务';
+$lang['tasks_page_intime']          = '打开此页面的任务：%1$d';
+$lang['tasks_page_late']            = '打开此页面的任务：%1$d，已延迟：%2$d';
+
+/** is not used
+$lang['discussion']                 = 'Discussion';
+$lang['back_to_article']            = 'Back to article';
+$lang['userpage']                   = 'User page';
+*/
+
+$lang['meta_data']                  = '元数据';
+$lang['osm_zoom_link_text']         = '查看更大的地图';
+$lang['osm_zoom_link_title']        = '在新窗口打开外部链接';
+
+$lang['image_detail']               = '图片详情';
+
+$lang['mode_edit']                  = '编辑页面';
+
+
+$lang['nav_sidebar']                = '站点导航';
+$lang['nav_sitemap']                = '站点地图';
+
+/**
+ * preferences colors
+ */
+
+$lang['__site_width__']             = 'content - width (in %)';
+
+$lang['__background_site__']        = 'website - background color';
+$lang['__text_webframe__']          = 'claim, title, footer (unlinked), navigation (unlinked) - font color';
+
+$lang['__nav_menu_color__']         = 'navigation (link), footer (link), pagetools icons, tabbox - font color';
+$lang['__nav_menu_hover_color__']   = 'navigation (hover) - font color, border color; wiki icons - font color; wiki icons (hover) - background color';
+$lang['__nav_menu_hover_bg__']      = 'navigation (hover) - background color; wiki icons - background color; wiki icons (hover) - font color';
+
+$lang['__background__']             = 'content, breadcrumb, pagetools (hover) - background color';
+$lang['__text__']                   = 'content: text - font color';
+
+$lang['__background_neu__']         = 'Content neutral (not too different from the content background) - background color';
+$lang['__text_neu__']               = 'content neutral: text - font color';
+
+$lang['__background_page_header__'] = 'content: header, footer, tabinclude tab controlls deactivate - background color';
+$lang['__background_page_footer__'] = 'content: footer, tabinclude tab controlls deactivate - font color';
+
+$lang['__border_light__']           = 'content: header, footer - border color';
+
+$lang['__highlight__']              = 'text emphasize (e.g. for search results) - background color';
+$lang['__highlight_text__']         = 'text emphasize (e.g. for search results) - font color';
+
+$lang['__existing__']               = 'links to wikipages, pencil, pagetools (hover) - font color; tabbox (hover) - font color, border color';
+$lang['__link__']                   = 'links to non wikipages (external links) - font color';
+$lang['__missing__']                = 'links to non-existing pages - font color';
+
+$lang['__button_color__']           = 'buttons - font color';
+$lang['__button_background__']      = 'buttons - background color';
+
+$lang['__background_alt__']         = 'table head, table cell (hover), struct Label (hover) - background color (alternative)';
+$lang['__text_alt__']               = 'table head: unlinked, table cell (hover), struct Label (hover) - font color (alternative)';
+$lang['__link_alt__']               = 'table head: linked - font color (alternative)';
+
+$lang['__border__']                 = 'tables, form fields, blockquotes - border color';
+$lang['__default_border_radius__']  = 'wiki icons, content (top right) - border radius';
